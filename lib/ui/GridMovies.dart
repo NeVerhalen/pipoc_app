c7d4a9002756a91dc4b38d962e2641a61e591c49
NeVerhalen <alineverhalen@gmail.com>
2019-03-08 15:28:30 -0400

    Imagens App

---
+++ lib/ui/GridMovies.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pipoc_app/ui/homePage.dart';
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter TabBar',
+      home: new Home(),
+      theme: new ThemeData(primaryColor: Colors.red),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    tabController = new TabController(length: 2, vsync: this);
+
+    var tabBarItem = new TabBar(
+      tabs: [
+        new Tab(
+          icon: new Icon(Icons.favorite_border),
+        ),
+        new Tab(
+          icon: new Icon(Icons.new_releases),
+        ),
+      ],
+      controller: tabController,
+      indicatorColor: Colors.white,
+    );
+
+    var gridView2 = new GridView.builder(
+        itemCount: 20,
+        gridDelegate:
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return new GestureDetector(
+            child: new Card(
+              elevation: 5.0,
+              child: new Container(
+                alignment: Alignment.center,
+                child: new Text('Item $index'),
+              ),
+            ),
+            onTap: () {
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                child: new CupertinoAlertDialog(
+                  title: new Column(
+                    children: <Widget>[
+                      new Text("GridView"),
+                      new Icon(
+                        Icons.favorite,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                  content: new Text("Selected Item $index"),
+                  actions: <Widget>[
+                    new FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: new Text("OK"))
+                  ],
+                ),
+              );
+            },
+          );
+        });
+
+    var gridView = new GridView.builder(
+        itemCount: 20,
+        gridDelegate:
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return new GestureDetector(
+            child: new Card(
+              elevation: 5.0,
+              child: new Container(
+                alignment: Alignment.center,
+                child: new Text('Item $index'),
+              ),
+            ),
+            onTap: () {
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                child: new CupertinoAlertDialog(
+                  title: new Column(
+                    children: <Widget>[
+                      new Text("GridView"),
+                      new Icon(
+                        Icons.favorite,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                  content: new Text("Selected Item $index"),
+                  actions: <Widget>[
+                    new FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: new Text("OK"))
+                  ],
+                ),
+              );
+            },
+          );
+        });
+
+    return new DefaultTabController(
+      length: 2,
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Flutter TabBar"),
+          bottom: tabBarItem,
+        ),
+        body: new TabBarView(
+          controller: tabController,
+          children: [
+            gridView2,
+            gridView,
+          ],
+        ),
+      ),
+    );
+  }
+}
